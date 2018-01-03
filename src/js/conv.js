3606e21a7a1a399b961aa6a7252994051afd5775
ugookoro <okorougoc@gmail.com>
2018-01-03 14:09:39 +0100

    Worked on theme for conv-js, added selectr.js dependency for searchable selects and select (multiple)

--- src/js/conv.js
+++ src/js/conv.js
@@ -1,5 +1,5 @@
 function conv(params) {
-    let questions = [];
+    var questions = [];
 
     (function getQuestions(doc) {
         var questionContainers = doc.getElementsByClassName(params.selector);
@@ -18,10 +18,52 @@ function conv(params) {
                     count++;
                     if (j === 0) {
                         questions[j].classList.add('active');
-                    }                   
-                }
+                    }   
+                    
+                    //var dropdowns = questions[j].getElementsByTagName('select');
+
+                    //for (var k = 0; k < dropdowns.length; k++) {                        
+                    //    var element = dropdowns[k];
+                    //    const sle = new Selectr(element);
+                    //}
+                    //     var displayDiv = doc.createElement('div');
+                    //     var selectionDiv = doc.createElement('div');                        
+                    //     var searchInput = doc.createElement('input');
+                    //     searchInput.setAttribute('type', 'text');
+                    //     const values = [];
+                    //     var itemList = doc.createElement('ul');
+                    //     var options = element.options;
+                    //     for (var l = 0; l < options.length; l++) {
+                    //         var listItem = doc.createElement('li');                                           
+                    //         var value = options[l].value;
+                    //         var text = options[l].innerText;                            
+                    //         values.push(text);
+                    //         listItem.setAttribute('data-conv-value', value);
+                    //         listItem.setAttribute('data-conv-text', text);
+                    //         listItem.innerText = text;
+                    //         itemList.appendChild(listItem);                
+                    //     }
+
+                    //     console.log(values);
+
+                    //     searchInput.addEventListener('keyup',function(e){
+                    //         var target = e.currentTarget;
+                    //         var searhText = target.value;
+                    //         console.log('Search', searhText);
+                            
+                    //     });
+                    //     selectionDiv.appendChild(searchInput);
+                    //     selectionDiv.appendChild(itemList);
+                    //     element.insertAdjacentElement('afterend',selectionDiv);
+                    //     element.insertAdjacentElement('afterend',displayDiv);
+                        
+                    // }
+                }   
             } else {
             }
+
+            console.log(questions);
+            
         }
 
     })(document);
@@ -45,13 +87,59 @@ function conv(params) {
             var currentActiveStep = currentActiveQuestion.getAttribute('data-step');
             var nextStep = Number(currentActiveStep) + 1;
             var nextQuestion = questionContainer.querySelectorAll('[data-step="'+nextStep+'"]')[0];
-            if (nextQuestion) {
-                currentActiveQuestion.classList.remove('active');
-                nextQuestion.classList.add('active');                
-            } else {
 
-                //submit the form because there's no next
-            }   
+            //if current active question is required, 
+            var questionHasValidation = currentActiveQuestion.hasAttribute('data-conv-validate');
+            if (questionHasValidation) {
+                var questionIsValid = validate(currentActiveQuestion.getAttribute('data-conv-validate'), currentActiveQuestion)
+                if (questionIsValid) {
+                    if (nextQuestion) {
+                        replace(questionContainer,currentActiveQuestion);
+                        currentActiveQuestion.classList.remove('active');
+                        nextQuestion.classList.add('active');                
+                    } else {
+        
+                        var containerType = questionContainer.tagName;
+                        switch (containerType.toLowerCase()) {
+                            case "form":
+                            questionContainer.submit();                                                                
+                                break;                
+                            default:
+                            var url = questionContainer.getAttribute('data-url');
+                            
+                                break;
+                        }
+                        console.log("Container type",containerType);
+        
+                        //submit the form because there's no next
+                    }   
+                }else{
+                    console.log('Question is InValid');
+                }
+            }else{
+                if (nextQuestion) {
+                    replace(questionContainer,currentActiveQuestion);                    
+                    currentActiveQuestion.classList.remove('active');
+                    nextQuestion.classList.add('active');                
+                } else {
+    
+                    var containerType = questionContainer.tagName;
+                    switch (containerType.toLowerCase()) {
+                        case "form":
+                        questionContainer.submit();                                                                
+                            break;                
+                        default:
+                        var url = questionContainer.getAttribute('data-url');
+                        
+                            break;
+                    }
+                    console.log("Container type",containerType);
+    
+                    //submit the form because there's no next
+                }  
+            }
+            
+           
         }
 
         if(element.classList.contains('conv-back')){
@@ -68,6 +156,58 @@ function conv(params) {
             }   
         }
     }, false);   
+
+    //Question Validator
+    function validate(validationType, question) {
+        var result = false;
+        switch (validationType.toLowerCase()) {
+            case 'required':                
+                var questionInput = question.querySelectorAll('input')[0] !== undefined ? question.querySelectorAll('input')[0] : question.querySelectorAll('select')[0];
+                if(questionInput.value !== undefined && questionInput.value !== null && questionInput.value !== ""){
+                    result = true;
+                }
+                break;
+        
+            default:
+                break;
+        }
+
+        return result
+    }
+
+    //Placeholder Replacement
+    function replace(questionContainer, currentActiveQuestion) {
+        var elementWithReplaceAttribute  = currentActiveQuestion.querySelectorAll('[data-conv-replace]')[0];
+        if (elementWithReplaceAttribute) {
+            var textToReplace = elementWithReplaceAttribute.getAttribute('data-conv-replace');
+            var replacementText = elementWithReplaceAttribute.value;
+            //get all placeholders in the container 
+            var placeHolders = questionContainer.querySelectorAll('[data-conv-place]');
+            for (i = 0; i < placeHolders.length; i++) {                
+                var element = placeHolders[i];
+                console.log('element', element);
+                if (element.getAttribute('data-conv-place') === textToReplace) {
+                    var str = element.innerText || element.textContent;
+                    element.innerText =  replacementText;
+                    element.textContent =  replacementText;
+                }else{
+
+                }                        
+            }
+            // var containerText = questionContainer.innerText;
+
+            // // containerText = containerText.replace('{{'+textToReplace+'}}', replacementText);
+
+            // // questionContainer.innerHTML = containerText;
+            // console.log(containerText);            
+
+            console.log('elementWithReplaceAttribute', elementWithReplaceAttribute);
+            console.log('textToReplace', textToReplace);
+            console.log('replacementText', replacementText);   
+        }  else{
+            console.log('Nothing to replace');
+        }              
+    }
 
 
 
